450b05495249cfc15a953319e07e4b36b3e7cdc4
hayley-d <hayleydod@proton.me>
2024-11-26 12:41:07 +0200

    enabled multiple senders

--- custom-channel/src/lib.rs
+++ custom-channel/src/lib.rs
@@ -2,82 +2,100 @@ use std::collections::VecDeque;
 use std::sync::{Arc, Condvar, Mutex};
 
 pub struct Sender<T> {
-    inner: Arc<Inner<T>>,
+    shared: Arc<Shared<T>>,
 }
 
 impl<T> Sender<T> {
     pub fn send(&mut self, t: T) {
-        let mut queue = self.inner.queue.lock().unwrap();
-        queue.push_back(t);
+        let mut inner = self.shared.inner.lock().unwrap();
+        inner.queue.push_back(t);
         // unlock the mutex
-        drop(queue);
+        drop(inner);
         // wake receiver waiting on a conditon
-        self.inner.not_empty.notify_one();
+        self.shared.not_empty.notify_one();
     }
 }
 
 impl<T> Clone for Sender<T> {
     fn clone(&self) -> Self {
+        let mut inner = self.shared.inner.lock().unwrap();
+        inner.senders += 1;
+        drop(inner);
         return Sender {
             // we want to clone the arc not the inner inside the arc
-            inner: Arc::clone(&self.inner),
+            shared: Arc::clone(&self.shared),
         };
     }
 }
 
+impl<T> Drop for Sender<T> {
+    fn drop(&mut self) {
+        let mut inner = self.shared.inner.lock().unwrap();
+        inner.senders -= 1;
+        if inner.senders == 0 {
+            self.shared.not_empty.notify_one();
+        }
+    }
+}
+
 pub struct Receiver<T> {
-    inner: Arc<Inner<T>>,
+    shared: Arc<Shared<T>>,
 }
 
 impl<T> Receiver<T> {
-    pub fn recv(&mut self) -> T {
-        let mut queue = self.inner.queue.lock().unwrap();
+    pub fn recv(&mut self) -> Option<T> {
+        let mut inner = self.shared.inner.lock().unwrap();
         loop {
-            match queue.pop_front() {
-                Some(t) => return t,
+            match inner.queue.pop_front() {
+                Some(t) => return Some(t),
+                None if inner.senders == 0 => return None,
                 None => {
                     // wait on the condition
                     // not a spin lock
-                    queue = self.inner.not_empty.wait(queue).unwrap();
+                    inner = self.shared.not_empty.wait(inner).unwrap();
                 }
             }
         }
     }
-
-    pub fn try_recv(&mut self) -> Option<T> {
-        let mut queue = self.inner.queue.lock().unwrap();
-        return queue.pop_front();
-    }
 }
-
-/*pub struct Inner<T> {
-    pub queue: Mutex<VecDeque<T>>,
-    pub not_empty: Condvar,
-}*/
 
 pub struct Inner<T> {
-    pub queue: Mutex<VecDeque<T>>,
+    pub queue: VecDeque<T>,
+    pub senders: usize,
+}
+
+pub struct Shared<T> {
+    pub inner: Mutex<Inner<T>>,
     pub not_empty: Condvar,
 }
 
-impl<T> Inner<T> {
-    pub fn new() -> Inner<T> {
-        return Inner {
-            queue: Mutex::new(VecDeque::new()),
+impl<T> Shared<T> {
+    pub fn new() -> Shared<T> {
+        return Shared {
+            inner: Mutex::new(Inner::new()),
             not_empty: Condvar::new(),
         };
     }
 }
 
+impl<T> Inner<T> {
+    pub fn new() -> Inner<T> {
+        return Inner {
+            queue: VecDeque::new(),
+            senders: 1,
+        };
+    }
+}
+
 pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
-    let inner: Inner<T> = Inner::new();
-    let inner = Arc::new(inner);
+    let shared: Shared<T> = Shared::new();
+    let shared = Arc::new(shared);
     return (
         Sender {
-            inner: inner.clone(),
+            shared: shared.clone(),
         },
         Receiver {
-            inner: inner.clone(),
+            shared: shared.clone(),
         },
     );
 }
@@ -90,6 +108,13 @@ mod tests {
     fn ping_pong() {
         let (mut tx, mut rx): (Sender<usize>, Receiver<usize>) = channel();
         tx.send(7);
-        assert_eq!(7, rx.recv());
+        assert_eq!(Some(7), rx.recv());
+    }
+
+    #[test]
+    fn closed() {
+        let (tx, mut rx) = channel::<()>();
+        drop(tx);
+        assert_eq!(rx.recv(), None);
     }
 }
