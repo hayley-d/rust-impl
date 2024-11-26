d6d0b001ae15f4af88270ba0cbd1da91e33856cd
hayley-d <hayleydod@proton.me>
2024-11-26 13:58:49 +0200

    refactor

--- synchronous-server/src/main.rs
+++ synchronous-server/src/main.rs
@@ -18,22 +18,23 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
 
-    // if the request is a GET
-    if buffer.starts_with(b"GET") {
-        let status_line = "HTTP/1.1 200 OK";
-        let contents = fs::read_to_string("html/index.html").unwrap();
-        let length = contents.len();
-
-        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-
-        stream.write_all(response.as_bytes()).unwrap();
+    if buffer.starts_with(b"GET / HTTP/1.1") {
+        format_response(
+            "HTTP/1.1 200 OK",
+            fs::read_to_string("html/index.html").unwrap(),
+            stream,
+        );
     } else {
-        let status_line = "HTTP/1.1 200 OK";
-        let contents = fs::read_to_string("html/hello.html").unwrap();
-        let length = contents.len();
-
-        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-
-        stream.write_all(response.as_bytes()).unwrap();
+        format_response(
+            "HTTP/1.1 200 OK",
+            fs::read_to_string("html/hello.html").unwrap(),
+            stream,
+        );
     }
+}
+
+fn format_response(status_line: &str, contents: String, mut stream: TcpStream) {
+    let length: usize = contents.len();
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    stream.write_all(response.as_bytes()).unwrap();
 }
