0f0d7366c33bfd5c9dafe9a34f5cae5b68b54898
hayley-d <hayleydod@proton.me>
2024-11-30 02:38:27 +0200

    part 3 complete

--- redis-rust/src/main.rs
+++ redis-rust/src/main.rs
@@ -1,21 +1,31 @@
 #![allow(unused_imports)]
-use std::io::Write;
+use std::io::{BufReader, Read, Write};
 use std::net::{TcpListener, TcpStream};
 
-fn main() {
+use anyhow::Error;
+
+fn main() -> Result<(), Error> {
+    println!("Listening on 127.0.0.1:6379");
+
     let listener: TcpListener = TcpListener::bind("127.0.0.1:6379").unwrap();
     for stream in listener.incoming() {
         match stream {
-            Ok(stream) => {
-                handle_client(stream);
+            Ok(mut stream) => {
+                println!("accepted new connection");
+                let mut buf = [0; 512];
+                loop {
+                    let read_count = stream.read(&mut buf).unwrap();
+                    if read_count == 0 {
+                        break;
+                    }
+                    stream.write(b"+PONG\r\n").unwrap();
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
-}
 
-fn handle_client(mut stream: TcpStream) {
-    let _ = stream.write(b"+PONG\r\n");
+    Ok(())
 }
